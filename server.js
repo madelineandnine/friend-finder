731b8b932ff7a17fda167ade7b64b1f6e70fa384
Madeline Wood <maddiewood42@gmail.com>
2018-05-01 19:02:34 -0400

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './app/public')));
 
